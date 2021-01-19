3c343adf3991f9340adbabf62dea58c3e08889ef
siarx <siarxxpl@gmai.com>
2021-01-19 16:50:33 +0100

    Addedd graph to graphs fragment

--- app/src/main/java/com/example/irrigationsystem/fragments/Graphs.kt
+++ app/src/main/java/com/example/irrigationsystem/fragments/Graphs.kt
@@ -1,17 +1,21 @@
 package com.example.irrigationsystem.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.irrigationsystem.R
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.BarGraphSeries
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+private lateinit var graph :GraphView
+
 class Graphs : Fragment() {
 
     override fun onCreateView(
@@ -19,7 +23,34 @@ class Graphs : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        graph = view.findViewById(R.id.mainGraph)
+
+        //Populate with Example Data
+        val series =
+            LineGraphSeries(
+                arrayOf<DataPoint>(
+                    DataPoint(0.0, 1.0),
+                    DataPoint(1.0, 5.0),
+                    DataPoint(2.0, 3.0),
+                    DataPoint(3.0, 2.0),
+                    DataPoint(4.0, 6.0)
+                )
+            )
+        series.color = Color.RED
+        val series2 =
+            LineGraphSeries(
+                arrayOf<DataPoint>(
+                    DataPoint(0.0,2.1),
+                    DataPoint(1.0,3.3),
+                    DataPoint(2.0,5.7),
+                    DataPoint(3.0,8.3),
+                    DataPoint(4.0,0.3)
+                )
+            )
+        graph.addSeries(series2)
+        graph.addSeries(series)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
